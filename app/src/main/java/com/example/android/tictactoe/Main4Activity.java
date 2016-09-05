7b14f78f7c00002eb705e0a45b793701f5dc81af
Nick Khachatryan <Nick Khachatryan>
2016-09-04 20:25:21 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/tictactoe/Main4Activity.java
@@ -0,0 +1,105 @@
+package com.example.android.tictactoe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main4Activity extends AppCompatActivity {
+
+
+
+    static int boardSize = 3;
+    static char[] mainBoard = new char[boardSize*boardSize];
+    List<Integer> list = new ArrayList<>(9);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        for (int i = 0; i < mainBoard.length; i++) {
+            mainBoard[i] = '*';
+            list.add(i);
+        }
+
+
+
+    }
+
+    public void clickedX(View v){
+
+        mainBoard[(v.getId()-R.id.imageViewX00)] = 'X';
+
+        ImageView xvisibility = (ImageView) findViewById(v.getId());
+        xvisibility.setImageResource(R.drawable.ximage);
+        xvisibility.setClickable(false);
+        list.remove((Object)(v.getId()-R.id.imageViewX00));
+
+
+        //Toast.makeText(this, Integer.toString(v.getId()), Toast.LENGTH_LONG).show();
+
+
+        if (checkWin('X')){
+            Toast.makeText(this, "**********\n"+"*YOU WIN*\n"+"**********", Toast.LENGTH_LONG).show();
+            MainActivity.playerScore ++;
+
+            MainActivity.gameScore.setText(MainActivity.playerScore+" : "+MainActivity.androidScore);
+            finish();
+            return;
+        }
+        if (list.isEmpty()){
+            Toast.makeText(this, "~~ DRAW ~~", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+
+
+
+
+
+        Random rand = new Random();
+        int rndIndexInList = rand.nextInt(list.size());
+        int temp = list.remove(rndIndexInList);
+
+        mainBoard[temp] = 'O';
+
+        ImageView ovisibility = (ImageView) findViewById(temp+R.id.imageViewX00);
+        ovisibility.setImageResource(R.drawable.oimage);
+        ovisibility.setClickable(false);
+
+        if (checkWin('O')){
+            Toast.makeText(this, "##########\n"+"# YOU LOSE #\n"+"##########", Toast.LENGTH_LONG).show();
+            MainActivity.androidScore ++;
+            MainActivity.gameScore.setText(MainActivity.playerScore+" : "+MainActivity.androidScore);
+            finish();
+            return;
+        }
+
+
+
+    }
+
+    public boolean checkWin (char who){
+        if ((mainBoard[0] == who)&&(mainBoard[1] == who)&&(mainBoard[2] == who)||
+            (mainBoard[3] == who)&&(mainBoard[4] == who)&&(mainBoard[5] == who)||
+            (mainBoard[6] == who)&&(mainBoard[7] == who)&&(mainBoard[8] == who)||
+            (mainBoard[0] == who)&&(mainBoard[3] == who)&&(mainBoard[6] == who)||
+            (mainBoard[1] == who)&&(mainBoard[4] == who)&&(mainBoard[7] == who)||
+            (mainBoard[2] == who)&&(mainBoard[5] == who)&&(mainBoard[8] == who)||
+            (mainBoard[0] == who)&&(mainBoard[4] == who)&&(mainBoard[8] == who)||
+            (mainBoard[2] == who)&&(mainBoard[4] == who)&&(mainBoard[6] == who)){
+            return true;
+        }
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/android/tictactoe/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.android.tictactoe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView player;
+    public static int playerScore = 0;
+    public static int androidScore = 0;
+    public static TextView gameScore;
+    EditText nameListener;
+    public static Editable playerName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gameScore = (TextView) findViewById(R.id.gameScore);
+        player = (TextView) findViewById(R.id.playerText);
+        nameListener = (EditText) findViewById(R.id.nameField);
+        gameScore.setText(playerScore+" : "+androidScore);
+        player.setText(playerName);
+
+
+        /*
+        nameListener.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                player.setText(nameListener.getText());
+
+            }
+        });
+        */
+
+
+
+    }
+
+    public void startButtonClick (View v) {
+
+        //Toast.makeText(this, "You clicked the Button", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(MainActivity.this, Main4Activity.class));
+
+    }
+
+    public void scoreReset (View v){
+        playerScore = 0;
+        androidScore = 0;
+        gameScore.setText(playerScore+" : "+androidScore);
+    }
+
+    public void saveName (View v){
+        playerName = nameListener.getText();
+        player.setText(playerName);
+        nameListener.setText("");
+        nameListener.setCursorVisible(false);
+    }
+
+}
